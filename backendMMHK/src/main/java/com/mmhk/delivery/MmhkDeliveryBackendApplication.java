37461264fe19d82fd24ff3b7f914408def972fbe
Hamzaessyagi <syagihamza3@gmail.com>
2025-12-07 03:25:42 +0100

    our first structure

---
+++ backendMMHK/src/main/java/com/mmhk/delivery/MmhkDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mmhk.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmhkDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmhkDeliveryBackendApplication.class, args);
+	}
+
+}
---
+++ backendMMHK/src/test/java/com/mmhk/delivery/MmhkDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmhk.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmhkDeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
