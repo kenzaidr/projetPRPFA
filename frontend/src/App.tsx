66f4b3b88fe46d162058f3c659dbd5fd973d7a35
keennyyyyyyyyyyy <k.idrissi0009@gmail.com>
2026-01-06 13:26:11 +0100

    t

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,7 @@ const App: React.FC = () => {
         <Route path="/driver/signup" element={<DriverSignup />} />
         <Route path="/driver/verification" element={<VerificationPage />} />
         <Route path="/partner" element={<PartnerLoginSignUP />} />
+        <Route path="/restaurant" element={<Restaurant />} />
         <Route path="/ride" element={<Ride />} />
         <Route path="/ride/booking" element={<RideBooking />} />
         <Route path="*" element={<Navigate to="/" replace />} />
